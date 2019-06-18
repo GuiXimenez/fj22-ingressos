5d8f285a8e882aa106d23d9b494943fec7f09ec1
Guilherme Ximenez <gasouza@icloud.com>
2019-06-18 15:22:00 -0300

    Disponibilizando Form para criação da sessão e salvando sessao no sistema

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,11 +2,24 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
 	public Sessao(LocalTime horario, Sala sala, Filme filme) {
@@ -15,15 +28,26 @@ public class Sessao {
 		this.sala = sala;
 		this.filme = filme;
 	}
+	
+	/**
+	 * @deprecated hibernate only
+	 */
+	public Sessao() {
+		
+	}
+	
 	public Integer getId() {
 		return id;
 	}
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
 	public LocalTime getHorario() {
 		return horario;
 	}
+	
 	public void setHorario(LocalTime horario) {
 		this.horario = horario;
 	}
